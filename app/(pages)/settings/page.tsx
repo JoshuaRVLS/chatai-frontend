d95f64f60be5e5b7c556cceae9b212b20c0c8558
Joshua Ravael <jravaellnew@gmail.com>
2026-01-08 21:28:47 +0700

    perf: remove inefficient server-side prefetching on settings page

--- app/(pages)/settings/page.tsx
+++ app/(pages)/settings/page.tsx
@@ -1,16 +1,7 @@
-import { QueryClient, usePrefetchQuery } from '@tanstack/react-query'
-import { getServerSession } from 'next-auth';
 import React from 'react'
 import Settings from '../components/Settings/Settings';
 
-const page = async () => {
-  const queryClient = new QueryClient();
-  const session = await getServerSession();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['settingsData'],
-    queryFn: () => fetch(`/api/users/${session?.user?.id}`).then(res => res.json())
-  })
+const page = () => {
   return (
     <Settings />
   )
