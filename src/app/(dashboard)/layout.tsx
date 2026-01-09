bc0ec2670adde7f217bc892c6bd7a758e04c9f16
Joshua Ravael <jravaellnew@gmail.com>
2026-01-09 19:18:01 +0700

    style(admin): increase spacing and gaps for better layout density

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
             <Sidebar />
             {/* Spacer for fixed sidebar */}
             <div className="w-[260px] shrink-0 hidden lg:block" />
-            <main className="flex-1 min-w-0 p-4 md:p-8 lg:p-12 relative z-0">
+            <main className="flex-1 min-w-0 p-6 md:p-12 lg:p-20 relative z-0">
                 {children}
             </main>
         </div>
