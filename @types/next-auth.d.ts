4b7386fd5cec1e342d909dd453e2da44100c52c7
Joshua Ravael <jravaellnew@gmail.com>
2026-01-09 13:19:07 +0700

    Fix type

--- @types/next-auth.d.ts
+++ @types/next-auth.d.ts
@@ -5,11 +5,13 @@ declare module "next-auth" {
     user: {
       id: string;
       username: string;
+      isAdmin: boolean;
     } & DefaultSession["user"];
   }
 
   interface User {
     id: string;
     username: string;
+    isAdmin: boolean;
   }
 }
